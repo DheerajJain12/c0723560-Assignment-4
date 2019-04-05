097e2428887ef0c0724bc932194ecc4e251402bc
visualstudio <visualstudio@122-PC38>
2019-04-05 18:10:18 -0400

    Starter-code

--- c0723560-Assignment-4/Program.cs
+++ c0723560-Assignment-4/Program.cs
@@ -3,14 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
 
-namespace c0723560_Assignment_4
+namespace c0732132_Assignment_4
 {
     class Program
     {
+        ArrayList Beowulf;
         static void Main(string[] args)
         {
-        }
+            Program p = new Program();
+            p.Beowulf = new ArrayList();
 
+        }
+        public void Run() { this.ReadTextFiles(); }
+
+        public void ReadTextFiles()
+        {
+            // Read file using StramReader. Read file line by line
+            using (StreamReader file = new StreamReader("U:/Users/723560/c0723560-Assignment-4/c0723560-Assignment-4/Beowulf.txt"))
+            {
+                int counter = 0;
+                string ln;
+
+                while ((ln = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(ln);
+                    Beowulf.Add(ln);
+
+                }
+                file.Close();
+                Console.WriteLine($"File has {counter} lines.");
+            }
+        }
+        public int FindNumberOfBlankSpaces(string line)
+        {
+            int countletters = 0;
+            int countSpaces = 0;
+
+            foreach (char c in line)
+            {
+                if (char.IsLetter(c))
+                {
+                    countletters++;
+                }
+                if (char.IsWhiteSpace(c))
+                {
+                    countSpaces++;
+                }
+                return countSpaces;
+            }
+        }
     }
 }
